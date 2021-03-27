d5bf06278973ac621c17727b744841d0137ccb54
dmiric <damir.miric@gmail.com>
2021-03-27 18:31:10 +0100

    new server paths

--- bots.config.js
+++ bots.config.js
@@ -4,14 +4,14 @@ module.exports = {
     script: "./app.js"
   },{
     name: "tBTCUSD",
-    script: "../bfx-bot/dist/main.js",
+    script: "../botV2/_work/bot/bot/dist/main.js",
     env: {
       "port": 3000,
       "symbol": "tBTCUSD"
     }
   },{
     name: "tETHUSD",
-    script: "../bfx-bot/dist/main.js",
+    script: "../botV2/_work/bot/bot/dist/main.js",
     env: {
       "port": 3001,
       "symbol": "tETHUSD"
